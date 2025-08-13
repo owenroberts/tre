61231c954c7ade7ddc9ef9cebca40e96aad38feb
Owen Roberts <obroberts@gmail.com>
2025-08-12 22:17:56 -0400

    adds vite setup, file re-org, staging

---
+++ vite.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+// need this to import shader files
+import vitePluginString from 'vite-plugin-string';
+
+export default defineConfig({
+	base: "./",
+	plugins: [vitePluginString()],
+	build: {
+		chunkSizeWarningLimit: 600,
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				staging: resolve(__dirname, 'staging/default.html'),
+			},
+			output: {
+				manualChunks: {
+					three: ['three'],
+				},
+			},
+		}
+	}
+});
