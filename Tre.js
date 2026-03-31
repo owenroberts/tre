d04885f0647c922238ac91e3a03b38ccd5197d9a
Owen Roberts <obroberts@gmail.com>
2026-03-30 22:01:46 -0400

    udpates imports

--- Tre.js
+++ Tre.js
@@ -1,10 +1,10 @@
 
-import { Animator } from './Animator.js';
-import { Joint } from './Joint.js';
-import { BandShader } from './BandShader.js';
-import { LinesPass } from './LinesPass.js';
-import vertexShader from './vert.glsl';
-import blendShader from './blend.glsl';
+import { Animator } from './src/Animator.js';
+import { Joint } from './src/Joint.js';
+import { BandShader } from './src/BandShader.js';
+import { LinesPass } from './src/LinesPass.js';
+import vertexShader from './glsl/vert.glsl';
+import blendShader from './glsl/blend.glsl';
 import { getArrowHelper, getTestCube, getAxesHelper } from './Helpers.js';
 
 export { Animator, Joint, BandShader, LinesPass, vertexShader, blendShader, getArrowHelper, getTestCube, getAxesHelper };
